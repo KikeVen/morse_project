5ec230a49e98a2223404410f24f9af0a4c5ca0af
KikeVen <bruzual.enrique@outlook.com>
2018-05-24 01:18:03 -0400

    edit docstring

--- 01_system_console.py
+++ 01_system_console.py
@@ -1,9 +1,6 @@
 """
 Import morse_pi and write an ui using the input() and print() methods. Encapsulate in while loop
 and add a condition to escape the loop
-1. You could write a function to print the Dictionary to screen and practice formating
-2. You could catch characters not in the dictionary and let the user know
-3. You can make your system read the returned morse and make the computer beep the morse code
 """
 import morsepi
 
